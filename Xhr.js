858862239e239255c4e212e9fd5ef52b7db0f4ca
Valerio <valerioneri.de@gmail.com>
2020-01-07 14:20:23 +0100

    updated with auth

--- Xhr.js
+++ Xhr.js
@@ -1,5 +1,5 @@
 var Xhr={	
-	GET:function(url, callback, param){
+	GET:function(url, callback, body, auth){
 		var xmlhttp;
 		if (window.XMLHttpRequest){// code for IE7+, Firefox, Chrome, Opera, Safari
 			xmlhttp=new XMLHttpRequest();
@@ -9,17 +9,19 @@ var Xhr={
 		
 		
 		xmlhttp.onreadystatechange=function(){
+			
 			if (xmlhttp.readyState==4 && xmlhttp.status==200){
-				callback(xmlhttp.responseText, param);
+				callback(xmlhttp.responseText);
 			}
 		}
 		
 		xmlhttp.open("GET",url,true);
-		//xmlhttp.setRequestHeader("Authorization", "Basic " + btoa('user' + ":" + 'pass'));
-		xmlhttp.send();
+		if (auth != undefined)
+			xmlhttp.setRequestHeader("Authorization", "Basic " + btoa(auth['username'] + ":" + auth['password']));
+		xmlhttp.send(body);
 	},
 
-	POST:function(url, parameters, callback, param){
+	POST:function(url, callback, body, auth, contentType){
 		var xmlhttp;
 		if (window.XMLHttpRequest){// code for IE7+, Firefox, Chrome, Opera, Safari
 			xmlhttp=new XMLHttpRequest();
@@ -30,12 +32,15 @@ var Xhr={
 		
 		xmlhttp.onreadystatechange=function(){
 			if (xmlhttp.readyState==4){
-				callback(xmlhttp.responseText, param);
+				callback(xmlhttp.responseText);
 			}
 		}
 		
 		xmlhttp.open("POST",url,true);
-		//xmlhttp.setRequestHeader("Authorization", "Basic " + btoa('user' + ":" + 'pass'));
-		xmlhttp.send(JSON.stringify(parameters));
+		if (contentType != undefined)
+			xmlhttp.setRequestHeader("Content-Type", contentType);
+		if (auth != undefined)
+			xmlhttp.setRequestHeader("Authorization", "Basic " + btoa(auth['username'] + ":" + auth['password']));
+		xmlhttp.send(JSON.stringify(body));
 	}
 }
